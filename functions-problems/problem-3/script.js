a7cfdbc35b2bfc263f7164239c2d1d49a6340c69
Peter Ogada <peterogada@Peters-MacBook-Air-2.local>
2023-02-02 21:30:08 -0500

    Redid the function problem without any looking at solution or googling commiting now

--- functions-problems/problem-3/script.js
+++ functions-problems/problem-3/script.js
@@ -3,24 +3,27 @@ number of occurrences of the specified letter within the string.
 
 1. I would define a Javascript function that takes in 2 parameters. 
 2. Define create a letterCount variable inside of the function and set it equal to 0.
-3. I would then proceed to write my for loop with its condition and logic.
-4. I would return the letterCount 
-5. I would console.log the function and input the 2 parameters in.
+3. I would then create an array and save the array in a variable.
+4. I would then proceed to write my for loop with its condition and logic.
+5. I would return the letterCount 
+6. I would console.log the function and input the 2 parameters in.
 */
 
-function numberOfOccurrences(string, letter) {
+function numberOfOccurrences (string, letter) {
+  let letters = string.toLowerCase().split('');
   let letterCount = 0;
-
-  for(let i = 0; i < string.length; i++) {
-    if(string.charAt(i) === letter) {
+  
+  for (let i = 0; i < letters.length; i++) {
+    if (letters [i] === letter) {
       letterCount += 1;
     }
   }
-
+  
   return letterCount;
+  
 }
 
-console.log(numberOfOccurrences('Hey my name is Peter', 'e'));
+console.log(numberOfOccurrences('Peter', 'e'));
 
 
 

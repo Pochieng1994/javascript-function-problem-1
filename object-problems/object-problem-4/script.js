132ed6a00544863f77a3e81b12e1a20bca734545
Peter Ogada <peterogada@Peters-MacBook-Air-2.local>
2023-03-21 21:40:58 -0400

    Finished Object Problem-4 pushing to github

--- object-problems/object-problem-4/script.js
+++ object-problems/object-problem-4/script.js
@@ -1,5 +1,11 @@
 /* Create an object, then write a JavaScript function that checks whether an object contains the 
 specified key.
+1. I would create an object.
+2. I would then define a function with 2 parameters object and key.
+3. I would finally pass an if else statement using the hasOwnProperty() Method this condition would check to
+see if a object that is passed in has a specified key. If it does then a message stating
+The Key exists in the object will display to the console if not then a message stating The Key does
+not exist in the object will display in the console. 
 */
 
 const countries = {
